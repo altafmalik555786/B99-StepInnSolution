9037eb92e0e544b9834f30ce38aba244ee93a6f2
Altaf Malik <altafmalik555786@gmail.com>
2023-03-06 12:47:33 +0500

    Implemented the configurations about buil using craco typesript

--- craco.config.js
+++ craco.config.js
@@ -5,12 +5,13 @@ module.exports = {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
+      "@components/*": path.resolve(__dirname, "src/components/*"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@utils": path.resolve(__dirname, "src/utils"),
-      // '@stores': path.resolve(__dirname, 'src/stores'),
-      // '@api': path.resolve(__dirname, 'src/api'),
+      '@stores': path.resolve(__dirname, 'src/stores'),
+      '@api': path.resolve(__dirname, 'src/api'),
       "@style": path.resolve(__dirname, "src/style"),
-      "@commonComponents": path.resolve(__dirname, "src/components/common"),
+      "@commonComponents": path.resolve(__dirname, "src/components/common-components"),
       "@commonComponentsScss": path.resolve(
         __dirname,
         "src/components/commonComponents/scss"
--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,13 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "*.tsx" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.ts" {
+  const content: Record<string, string>;
+  export default content;
+}
